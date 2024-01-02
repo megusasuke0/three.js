40cc3c2945916afd03272dbd0202983bf64f049c
megusasuke0 <116707505+megusasuke0@users.noreply.github.com>
2024-01-02 19:36:17 +0900

    add new file

--- main.js
+++ main.js
@@ -1,39 +1,40 @@
-import './style.css';
+import './style.css'
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-// Setup
+//setup---------------------------
 
-const scene = new THREE.Scene();
+let scene , camera , renderer;
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const renderer = new THREE.WebGLRenderer({
+//❶シーンを追加
+scene = new THREE.Scene();
+//❷カメラを追加
+camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+//❸レンダラーを追加 
+renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
+//レンダラーのサイズを設定 ...setSize(画面幅,画面の高さ)
+renderer.setSize(window.innerWidth, window.innerHeight)
+//どこにレンダラーを挿入するか？
+//document.body.appendChild(renderer.domElement);
 
+//①ジオメトリーを作成
+let ballGeometry = new THREE.SphereGeometry(100, 64, 32);
+//②マテリアルを作成
+let ballMaterial = new THREE.MeshPhysicalMaterial();
+//③メッシュ化してみよう。ジオメトリー　＋　マテリアル
+let ballMesh = new THREE.Mesh(ballGeometry,ballMaterial) ;
+//④メッシュ化したものをsceneに追加
+scene.add(ballMesh);
+
+//⑤カメラの位置を調整
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 camera.position.setX(-3);
 
+//⑥レンダリングしてみよう
 renderer.render(scene, camera);
-
-// Torus
-
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
-const torus = new THREE.Mesh(geometry, material);
-
-scene.add(torus);
-
-// Lights
-
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5, 5, 5);
-
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight, ambientLight);
 
 // Helpers
 
@@ -41,90 +42,4 @@ scene.add(pointLight, ambientLight);
 // const gridHelper = new THREE.GridHelper(200, 50);
 // scene.add(lightHelper, gridHelper)
 
-// const controls = new OrbitControls(camera, renderer.domElement);
-
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const star = new THREE.Mesh(geometry, material);
-
-  const [x, y, z] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(100));
-
-  star.position.set(x, y, z);
-  scene.add(star);
-}
-
-Array(200).fill().forEach(addStar);
-
-// Background
-
-const spaceTexture = new THREE.TextureLoader().load('space.jpg');
-scene.background = spaceTexture;
-
-// Avatar
-
-const jeffTexture = new THREE.TextureLoader().load('jeff.png');
-
-const jeff = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: jeffTexture }));
-
-scene.add(jeff);
-
-// Moon
-
-const moonTexture = new THREE.TextureLoader().load('moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('normal.jpg');
-
-const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(3, 32, 32),
-  new THREE.MeshStandardMaterial({
-    map: moonTexture,
-    normalMap: normalTexture,
-  })
-);
-
-scene.add(moon);
-
-moon.position.z = 30;
-moon.position.setX(-10);
-
-jeff.position.z = -5;
-jeff.position.x = 2;
-
-// Scroll Animation
-
-function moveCamera() {
-  const t = document.body.getBoundingClientRect().top;
-  moon.rotation.x += 0.05;
-  moon.rotation.y += 0.075;
-  moon.rotation.z += 0.05;
-
-  jeff.rotation.y += 0.01;
-  jeff.rotation.z += 0.01;
-
-  camera.position.z = t * -0.01;
-  camera.position.x = t * -0.0002;
-  camera.rotation.y = t * -0.0002;
-}
-
-document.body.onscroll = moveCamera;
-moveCamera();
-
-// Animation Loop
-
-function animate() {
-  requestAnimationFrame(animate);
-
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
-
-  moon.rotation.x += 0.005;
-
-  // controls.update();
-
-  renderer.render(scene, camera);
-}
-
-animate();
+//setupここまで---------------------------
